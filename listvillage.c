64f1e9c792fa31dfd1a363f332b6bc3b05ace24e
Restu Wahyu Kartiko <31585183+rwk03@users.noreply.github.com>
2017-11-24 13:36:49 +0700

    BARU BOY. TOLONG DI BACA

--- listvillage.c
+++ listvillage.c
@@ -52,19 +52,24 @@ addressVillage SearchVil (ListVil L, Village X)
 
 addressVillage SearchKoordinatVil (ListVil L, POINT Koordinat){
 	addressVillage P;
+	boolean found = false;
 	if (IsEmptyVil(L)) {
 		return Nil;
 	} else {
 		P = FirstVillage(L);
-		do {
+		while (NextVillage(P) != FirstVillage(L) && !found) {
 			if((PosX(InfoVillage(P))) == Absis(Koordinat) && PosY(InfoVillage(P)) == Ordinat(Koordinat)){
 				return P;
 			}
 			else {
 				P = NextVillage(P);
 			}
-		} while(P != FirstVillage(L));
-		return Nil;
+		}
+		if ((PosX(InfoVillage(P))) == Absis(Koordinat) && PosY(InfoVillage(P)) == Ordinat(Koordinat)){
+			return P;
+		} else {
+			return Nil;
+		}
 	}
 }
 
